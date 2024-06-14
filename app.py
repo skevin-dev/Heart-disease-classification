504fcf8b5ea30e2b10b47dcc5338e348ee602a19
shy2351783 <Kevin.Shyaka@rra.gov.rw>
2024-06-14 12:19:09 +0200

    add requirements

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
 import pickle
+from pathlib import Path
 from sklearn.preprocessing import MinMaxScaler
 
 
-model_path = 'models/XGBoost.pkl'
+model_path = Path(__file__).parent / 'models/XGBoost.pkl'
 with open(model_path, 'rb') as f:
     model = pickle.load(f)
 
